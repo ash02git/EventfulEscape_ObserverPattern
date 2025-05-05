5173361690a32f17c115721e8f165af06cb4617d
Ashwin02 <ashwinabraham2001@gmail.com>
2025-05-05 14:46:41 +0530

    Made code with better practices

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -12,9 +12,11 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     private void Start() => currentState = SwitchState.Off;
 
     private void OnEnable()=> lightSwitch += OnLightSwitchToggled;
+
+    private void OnDisable()=> lightSwitch -= OnLightSwitchToggled;
     public void Interact()
     {
-        lightSwitch.Invoke();
+        lightSwitch?.Invoke();
     }
     private void toggleLights()
     {
