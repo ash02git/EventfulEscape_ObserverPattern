0c0cb39a328ce09bc99b6eeeac3ae8e5d958e125
Ashwin02 <ashwinabraham2001@gmail.com>
2025-05-05 15:41:06 +0530

    Converted delegate to event

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,7 +17,6 @@ public class PlayerController
     public int KeysEquipped { get => playerScriptableObject.KeysEquipped; set => playerScriptableObject.KeysEquipped = value; }
     public PlayerState PlayerState { get => playerState; private set => playerState = value; }
 
-
     public PlayerController(PlayerView playerView, PlayerScriptableObject playerScriptableObject)
     {
         this.playerView = playerView;
@@ -28,13 +27,11 @@ public class PlayerController
         LightSwitchView.lightToggled += onLightSwitch;
         playerState = PlayerState.InDark;
     }
-
     ~PlayerController()
     {
         LightSwitchView.lightToggled -= onLightSwitch;
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
-
     public void Jump(Rigidbody playerRigidbody, Transform transform)
     {
         bool IsGrounded = Physics.Raycast(transform.position, -transform.up, playerScriptableObject.raycastLength);
@@ -61,7 +58,6 @@ public class PlayerController
     {
         PlayerState = PlayerState.Dead;
     }
-
     private void GetInput()
     {
         horizontalAxis = Input.GetAxis("Horizontal");
