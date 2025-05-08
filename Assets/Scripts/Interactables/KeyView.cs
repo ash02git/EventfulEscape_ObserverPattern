b254161ff3f529176fa8a65a35b04e354cbdfa92
Ashwin02 <ashwinabraham2001@gmail.com>
2025-05-08 14:44:18 +0530

    Improved UI logic for keys by elimination serialised reference tto GameViewUI

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour, IInteractable
 {
-    [SerializeField] GameUIView gameUIView;
+    //[SerializeField] GameUIView gameUIView;
     public void Interact()
     {
+        int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
+
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
         GameService.Instance.GetPlayerController().KeysEquipped++;
-        gameUIView.UpdateKeyText();
+        //gameUIView.UpdateKeyText();
+
+        currentKeys++;
+
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(currentKeys);
 
         gameObject.SetActive(false);
     }
