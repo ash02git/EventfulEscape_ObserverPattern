9f869e84919f4cb7e91bb4e9bfa3c3a40325b9b6
Ashwin02 <ashwinabraham2001@gmail.com>
2025-05-11 16:16:59 +0530

    Added potionDrink Event

--- Assets/Scripts/Service/EventService.cs
+++ Assets/Scripts/Service/EventService.cs
@@ -21,6 +21,7 @@ public class EventService
     public EventController PlayerDeathEvent { get; private set; }
     public EventController OnRatRush {  get; private set; }
     public EventController OnSkullDrop {  get; private set; }
+    public EventController<int> OnPotionDrink { get; private set; }
 
     public EventService()
     {
@@ -29,6 +30,7 @@ public class EventService
         OnLightsOffByGhostEvent = new EventController();
         OnRatRush = new EventController();
         OnSkullDrop = new EventController();
+        OnPotionDrink = new EventController<int>();
 
         PlayerEscapedEvent = new EventController();
         PlayerDeathEvent = new EventController();
